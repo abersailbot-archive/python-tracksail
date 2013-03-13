8d56d166e8a366ffbf9197bd1164b199db4bba9d
Louis Taylor <kragniz@gmail.com>
2013-03-13 23:20:59 +0000

    Added some properties

--- tracksail.py
+++ tracksail.py
@@ -1,5 +1,12 @@
 import socket
 
+def _float(v):
+    print 'coverting "%s"' % v
+    if v:
+        return float(v[:-1])
+    else:
+        return None
+
 class Tracksail(object):
     def __init__(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,8 +21,29 @@ class Tracksail(object):
     def close(self):
         self._socket.close()
 
+    @property
+    def windDirection(self):
+        return _float(self._send_command('get wind_dir'))
+
+    @property
+    def compass(self):
+        return _float(self._send_command('get compass'))
+
+    @property
+    def sail(self):
+        return _float(self._send_command('get sail'))
+
+    @sail.setter
+    def sail(self, value):
+        print 'set sail {}'.format(int(value))
+        self._send_command('set sail {}'.format(int(value)))
+
+
 if __name__ == '__main__':
     t = Tracksail()
     t.connect()
-    print t._send_command('get wind_dir')
+    print t.windDirection
+    print t.sail
+    t.sail = 10
+    print t.sail
     t.close()
